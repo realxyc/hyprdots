7e81859d1659fbfe2013090457bd1bd55418a2e3
amit-0i <6329amit.cs@gmail.com>
2025-09-16 00:01:07 +0530

    refactor: change spotify package and update theme

--- Configs/.local/share/bin/wallbashspotify.sh
+++ Configs/.local/share/bin/wallbashspotify.sh
@@ -9,13 +9,7 @@ source "${scrDir}/globalcontrol.sh"
 
 # regen conf
 
-if pkg_installed spotify && pkg_installed spicetify-cli ; then
-
-    if [ ! -w /opt/spotify ] || [ ! -w /opt/spotify/Apps ]; then
-        notify-send -a "Hyprdots" "Permission needed for Wallbash Spotify theme"
-        pkexec chmod a+wr /opt/spotify
-        pkexec chmod a+wr /opt/spotify/Apps -R
-    fi
+if pkg_installed spotify-launcher && pkg_installed spicetify-cli ; then
 
     if [ "$(spicetify config | awk '{if ($1=="color_scheme") print $2}')" != "Wallbash" ] || [[ "${*}" == *"--reset"*  ]] ; then
         spicetify &> /dev/null
@@ -25,7 +19,7 @@ if pkg_installed spotify && pkg_installed spicetify-cli ; then
         spotfy_flags='--ozone-platform=wayland'
         sed -i -e "/^prefs_path/ s+=.*$+= $HOME/.config/spotify/prefs+g" \
             -e "/^spotify_launch_flags/ s+=.*$+= $spotfy_flags+g" "$sptfyConf"
-	    curl -L -o ${cacheDir}/landing/Spotify_Sleek.tar.gz https://github.com/prasanthrangan/hyprdots/raw/main/Source/arcs/Spotify_Sleek.tar.gz
+	    curl -L -o ${cacheDir}/landing/Spotify_Sleek.tar.gz https://github.com/amit-0i/hyprdots/raw/main/Source/arcs/Spotify_Sleek.tar.gz
         tar -xzf ${cacheDir}/landing/Spotify_Sleek.tar.gz -C ~/.config/spicetify/Themes/
         spicetify backup apply
         spicetify config current_theme Sleek
