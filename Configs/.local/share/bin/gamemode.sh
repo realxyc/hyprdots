d41f087ef93e04b226f4a8acef334d575ec61a26
amit-0i <6329amit.cs@gmail.com>
2025-09-16 23:52:50 +0530

    feat: non-transparent windows in gamemode

--- Configs/.local/share/bin/gamemode.sh
+++ Configs/.local/share/bin/gamemode.sh
@@ -22,7 +22,8 @@ if [ $HYPRGAMEMODE = 1 ]; then
         keyword general:gaps_in 0;\
         keyword general:gaps_out 0;\
         keyword general:border_size 1;\
-        keyword decoration:rounding 0"
+        keyword decoration:rounding 0;\
+        keyword windowrule opaque,class:.*"
 	exit
 else
 	hyprctl reload
